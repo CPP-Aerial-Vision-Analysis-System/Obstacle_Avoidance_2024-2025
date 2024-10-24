84fbf1f8ae8c9a57cce56725b055d56403267c36
ethan-ngo <ethanngo3539@gmail.com>
2024-10-23 22:23:46 -0700

    created new ros environment

---
+++ Homebrew-OA/catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/src/beginner_tutorials
+# Build directory: /home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Homebrew-OA/catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
