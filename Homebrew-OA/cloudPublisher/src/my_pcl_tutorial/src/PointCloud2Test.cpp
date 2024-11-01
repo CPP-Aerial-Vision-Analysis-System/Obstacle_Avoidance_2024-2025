725deca139c7e2582795bb3535c3f052adbdc4f9
pixelatedknight27 <maxgross72@gmail.com>
2024-11-01 02:49:58 -0700

    added ros vscode config in local folder and the point cloud shows up in RVIS

--- Homebrew-OA/cloudPublisher/src/my_pcl_tutorial/src/PointCloud2Test.cpp
+++ Homebrew-OA/cloudPublisher/src/my_pcl_tutorial/src/PointCloud2Test.cpp
@@ -11,34 +11,37 @@ using namespace ros;
 
 int main(int argc, char **argv){
 	// Initialzie ROS, allows remapping through command line
-	ros::init(argc, argv, "talker");
+	ros::init(argc, argv, "chatter");
 
 	ros::NodeHandle n;
 
 	// Rate object specifies frequency to loop at
-	ros::Rate loopRate(10);
+	ros::Rate loopRate(1);
 
 	// Returns a ros Publisher object
-	ros::Publisher lidarScanPub = n.advertise<std_msgs::String>("chatter", 10);
+	ros::Publisher lidarScanPub = n.advertise<sensor_msgs::PointCloud2>("chatter", 10);
+
+	srand(time(0));
 
 	while(ros::ok()){
 
-		ROS_DEBUG("Hello %s", "World");
 		// Create pointcloud object
 		pcl::PointCloud<pcl::PointXYZ> cloud;
 		
 		// Fill cloud with random points
-		for (int i = 0; i < 1000; i++){
+		for (int i = 0; i < 256; i++){
 			pcl::PointXYZ newPoint;
-			newPoint.x = (rand() * 100.0) / RAND_MAX;
-			newPoint.y = (rand() * 100.0) / RAND_MAX;
-			newPoint.z = (rand() * 100.0) / RAND_MAX;
+			newPoint.x = ((float)rand()) / (float)RAND_MAX;
+			newPoint.y = ((float)rand()) / (float)RAND_MAX;
+			newPoint.z = ((float)rand()) / (float)RAND_MAX;
 			cloud.points.push_back(newPoint);
 		}
 
 		// Convert to sensor_msgs::PointCloud2 object
 		sensor_msgs::PointCloud2 cloud_msg;
 		pcl::toROSMsg(cloud, cloud_msg);
+
+		cloud_msg.header.frame_id = "map";
 
 		// Publish point cloud
 		lidarScanPub.publish(cloud_msg);
