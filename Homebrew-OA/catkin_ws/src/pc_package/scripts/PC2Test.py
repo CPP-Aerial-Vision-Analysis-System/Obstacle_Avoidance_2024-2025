7ce40a0c38c02ba3097fae7a7d07929c732996bd
ethan-ngo <ethanngo3539@gmail.com>
2024-11-02 00:10:55 -0700

    updated python pointcloud script

--- Homebrew-OA/catkin_ws/src/pc_package/scripts/PC2Test.py
+++ Homebrew-OA/catkin_ws/src/pc_package/scripts/PC2Test.py
@@ -9,36 +9,28 @@ import random
 def create_random_pointcloud():
     # Create an empty list to hold points
     points = []
-    for _ in range(1000):
+    for _ in range(256):
         # Generate random x, y, z coordinates
-        x = random.uniform(0, 100.0)
-        y = random.uniform(0, 100.0)
-        z = random.uniform(0, 100.0)
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        z = random.uniform(0, 1)
         points.append([x, y, z])
     
-    # Define fields for PointCloud2
-    fields = [
-        PointField('x', 0, PointField.FLOAT32, 1),
-        PointField('y', 4, PointField.FLOAT32, 1),
-        PointField('z', 8, PointField.FLOAT32, 1)
-    ]
-    
     # Create a PointCloud2 message
-    cloud_msg = pc2.create_cloud(rospy.Header(frame_id="lidar_frame"), fields, points)
+    cloud_msg = pc2.create_cloud_xyz32(rospy.Header(frame_id="map"), points)
     return cloud_msg
 
 def talker():
     # Initialize the ROS node
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('chatter', anonymous=True)
 
     # Publisher for the PointCloud2 topic
-    lidar_scan_pub = rospy.Publisher('chatter', PointCloud2, queue_size=10)
+    lidar_scan_pub = rospy.Publisher('pointcloud', PointCloud2, queue_size=10)
 
     # Set loop rate to 10Hz
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(1)
     
     while not rospy.is_shutdown():
-        rospy.loginfo("Inside")
         # Create random point cloud
         cloud_msg = create_random_pointcloud()
         
