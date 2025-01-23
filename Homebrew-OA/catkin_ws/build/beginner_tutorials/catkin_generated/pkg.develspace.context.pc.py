f6d5d0c0a79b196518d00c46513c167a9002f1d6
pixelatedknight27 <maxgross72@gmail.com>
2025-01-22 19:02:39 -0800

    ignored build folder

--- Homebrew-OA/catkin_ws/build/beginner_tutorials/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "beginner_tutorials"
-PROJECT_SPACE_DIR = "/home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- Homebrew-OA/catkin_ws/build/pc_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/src/pc_package
-# Build directory: /home/ethan/Projects/Obstacle_Avoidance_2024-2025/Homebrew-OA/catkin_ws/build/pc_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
